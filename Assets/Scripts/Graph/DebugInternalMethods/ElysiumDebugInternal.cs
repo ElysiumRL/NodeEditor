93e91a0db2adf6bbac74da38e484a0514691bf9b
Elysium <48912327+ElysiumRL@users.noreply.github.com>
2023-05-31 22:15:58 +0200

    Recompiled Node scripts

--- Assets/Scripts/Graph/DebugInternalMethods/ElysiumDebugInternal.cs
+++ Assets/Scripts/Graph/DebugInternalMethods/ElysiumDebugInternal.cs
@@ -2,32 +2,27 @@ using UnityEngine;
 
 namespace ElysiumGraphs
 {
-	public class ElysiumDebugInternal
+public class ElysiumDebugInternal
+{
+	public void Log(string format)
 	{
-		public void Log(string format)
-		{
-			Debug.Log(format);
-		}
-		public void LogWarning(string format)
-		{
-			Debug.LogWarning(format);
-		}	
-		public void LogError(string format)
-		{
-			Debug.LogError(format);
-		}	
-		public void LogAssert(string format)
-		{
-			Debug.LogAssertion(format);
-		}
-
-		public string Test()
-		{
-			return "test";
-		}
-		public string Test4()
-		{
-			return "test88";
-		}
+		Debug.Log(format);
 	}
+	public void LogWarning(string format)
+	{
+		Debug.LogWarning(format);
+	}	
+	public void LogError(string format)
+	{
+		Debug.LogError(format);
+	}	
+	public void LogAssert(string format)
+	{
+		Debug.LogAssertion(format);
+	}
+	public string StringRetvalTest()
+	{
+		return "String Return Value";
+	}
+}
 }
